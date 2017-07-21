c88950e94758568269c733a7a40f3cbdbac9dcd9
Bruno Augusto <bruno767@gmail.com>
2017-07-21 16:12:32 +0100

    New feature added Refresh loadingScreen

--- QuestionList/QuestionList/LoadingView/LoadingViewController.swift
+++ QuestionList/QuestionList/LoadingView/LoadingViewController.swift
@@ -14,25 +14,39 @@ class LoadingViewController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var activity: UIActivityIndicatorView!
 
+    @IBOutlet weak var refreshButton: UIButton!
     let apiHelper = APIHelper()
     
     override func viewDidLoad() {
         super.viewDidLoad()    
   
         self.activity.startAnimating()
-        
         if #available(iOS 10.0, *) {
             self.startTestConnection()
         } else {
-            // Fallback on earlier versions
+            self.showAlertMessage(titleStr: "iOS deprecated", messageStr: "To use a AlertView you must have iOS 10+.")
         }
+        //self.refreshButton.isEnabled = false
+        self.refresh(UIButton())
         
         
+    }
+    @IBAction func refresh(_ sender: Any) {
+        self.activity.startAnimating()
         self.statusLabel.text = "Checking connection.."
         apiHelper.isHealth { (status,error) in
-            self.performSegue(withIdentifier: "GoToQuestionsList", sender: nil)
-            self.activity.stopAnimating()
+            if status {
+                self.performSegue(withIdentifier: "GoToQuestionsList", sender: nil)
+                self.activity.stopAnimating()
+            }else{
+                self.statusLabel.text = "Connection Fail, please try again..."
+                self.refreshButton.isEnabled = true
+            }
         }
+    }
+    
+    func isHealth(){
+        
     }
 
 }
